df883dc5c9b7f2c92d3fb85f2b4899a1a3d97632
Sinsin9797 <Singhjhoom4@gmail.com>
2025-04-06 12:25:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function showResult() {
   const normalizedAngle = angle % (2 * Math.PI);
   const segmentAngle = (2 * Math.PI) / segments.length;
-  const index = Math.floor(((2 *  - normalizedAngle + segmentAngle / 2) % (2 * Math.PI)) / segmentAngle);
+  const index = Math.floor(((2 * Math.PI - normalizedAngle + segmentAngle / 2) % (2 * Math.PI)) / segmentAngle);
 
   if (segments[index]) {
     document.getElementById("result").innerText = "You won: " + segments[index];
